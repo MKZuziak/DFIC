5bc8dda60be95be2091447db370e9c5f66e66cee
MKZuziak <maciejzuziak101@gmail.com>
2024-10-21 12:00:14 +0200

    generated dataset for all 3 federated scenarios

---
+++ dfic/cifar10/federated/dataset/generate_dataset.py
@@ -0,0 +1,23 @@
+from fedata.hub.generate_dataset import generate_dataset
+import os
+
+def main():
+    data_config = {
+    "dataset_name" : "cifar10",
+    "split_type" : "dirchlet",
+    "shards": 10,
+    "local_test_size": 0.2,
+    "transformations": {},
+    "imbalanced_clients": {},
+    "save_dataset": True,
+    "save_transformations": True,
+    "save_blueprint": True,
+    "agents": 10,
+    "shuffle": True,
+    "alpha": 0.5,
+    "save_path": os.getcwd()}
+    generate_dataset(config=data_config)
+
+
+if __name__ == "__main__":
+    main()
